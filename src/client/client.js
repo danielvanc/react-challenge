6b796e7e75f9a50481f2b5e4959f3ab096c987d0
danielvanc <email@danielvanc.com>
2021-08-31 20:37:43 +0100

    feat: Added client utils

---
+++ src/client/client.js
@@ -0,0 +1,32 @@
+import config from '../config';
+
+export async function client(url, { data, method, headers: customHeaders, ...customConfig } = {}) {
+  const config = {
+    method,
+    body: data ? JSON.stringify(data) : "",
+    url,
+    headers: {
+      ...customHeaders,
+    },
+    ...customConfig
+  }  
+  
+  return await fetch(url, config)
+      .then(res => res.json())
+      .then((data) => data)
+}
+
+export async function fireFetch(api = "") {
+  const auth = window.btoa(`${config.api.clientId}:${config.api.clientSecret}`)
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Authorization': `Basic ${auth}`,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    "body": "grant_type=client_credentials"
+  }
+  
+  return await client(api, options)
+}
