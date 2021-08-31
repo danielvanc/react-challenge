9f3e155abe68f277503e5966d7ae94483aa7879e
danielvanc <email@danielvanc.com>
2021-08-31 20:36:47 +0100

    chore: Converted class comp to function comp

--- src/routes/Discover/components/Discover.js
+++ src/routes/Discover/components/Discover.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
 import '../styles/_discover.scss';
+import { useGetData } from '../../../hooks/hooks'
 
-export default class Discover extends Component {
-  constructor() {
-    super();
 
-    this.state = {
-      newReleases: [],
-      playlists: [],
-      categories: []
-    };
-  }
+export default function Discover() {
+  
+  const [newReleases, setNewReleases] = useState([]);
+  const [playlists, setPlaylists] = useState([]);
+  const [categories, setCategories] = useState([]);
+  
+  // const [{ releases, playlists, categories }] = useGetData();
+  useGetData();
+  
 
-  render() {
-    const { newReleases, playlists, categories } = this.state;
 
-    return (
-      <div className="discover">
-        <DiscoverBlock text="RELEASED THIS WEEK" id="released" data={newReleases} />
-        <DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
-        <DiscoverBlock text="BROWSE" id="browse" data={categories} imagesKey="icons" />
-      </div>
-    );
-  }
+  return (
+    <div className="discover">
+      <DiscoverBlock text="RELEASED THIS WEEK" id="released" data={newReleases} />
+      <DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
+      <DiscoverBlock text="BROWSE" id="browse" data={categories} imagesKey="icons" />
+    </div>
+  )
 }
